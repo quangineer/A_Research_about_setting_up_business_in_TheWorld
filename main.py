1fbcf6d5c4948c844a28030853ede063020cb196
Quan Nguyen <quangineer@gmail.com>
2019-06-03 22:57:05 -0700

    starter

---
+++ main.py
@@ -0,0 +1,12 @@
+import pandas as pd 
+
+# Read file excel
+data_xls_Cost = pd.read_excel('CostofbizstartupprocedureinpercentofGNIpercapita.xlsx', index_col=None)
+data_xls_Disclosure = pd.read_excel('bizextentdisclosureindex.xlsx', index_col=None)
+data_xls_Ease = pd.read_excel('easeofdoingbusiness.xlsx', index_col=None)
+
+
+# Convert TO CSV
+data_xls_Cost.to_csv('Cost.csv', encoding='utf-8', index=False)
+data_xls_Disclosure.to_csv('Disclosure.csv', encoding='utf-8', index=False)
+data_xls_Ease.to_csv('Ease.csv', encoding='utf-8', index=False)
