6d9e7c27bf6358c51b479a32a3b9dc347c294107
Quan Nguyen <quangineer@gmail.com>
2019-06-04 21:02:43 -0700

    updates

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pandas as pd 
+import numpy as np
 import matplotlib as plt
 from matplotlib import pyplot
+
 
 # Read file excel
 data_xls_Cost = pd.read_excel('CostofbizstartupprocedureinpercentofGNIpercapita.xlsx', index_col=None)
@@ -17,16 +19,24 @@ df1 = pd.read_csv("Cost.csv")
 df2 = pd.read_csv("Disclosure.csv")
 df3 = pd.read_csv("Ease.csv")
 
-# basic command for df1:
-# print (df1.shape)
-# print (df1.info())
-# print (df1.describe())
-# print (df1.dtypes)
-# print (df1.isnull().sum())
-# print (df1[df1.isnull()])
-# print (df1[df1['2017'].notnull()])
-# print (df1.isnull())
-# print(df1[["2013","2014","2015","2016","2017"]].isnull().sum())
-# print (df1.loc[:,"country"])
-# print (df1[df1["2010"].isnull()])
-# print (df1["2013"].isnull().sum())
+# # print(df1["2017"])
+# df1.hist(color='b', label='costbiz2017')
+# plt.pyplot.legend(loc='upper right')
+# plt.pyplot.title('BusinessSetup2017')
+# plt.pyplot.xlabel('country')
+# plt.pyplot.ylabel('Cost')
+# plt.pyplot.show()
+
+# print (df1["2017"].nlargest(20))
+# print (df1[(df1["2003"].nlargest(20))])
+
+# print(df1.nlargest(20, ["2003"]))
+# print(df1.nsmallest(20, ["2017"]))
+# print (df1["2003"])
+# print (df1[["2003","country"]].nlargest(20, "2003"))
+
+a = (df1[["2003","country"]].nlargest(20, "2003"))
+a.plot(kind='bar',x='country',y='2003', color='y', label='Cost2003')
+plt.pyplot.legend(loc='upper right')
+plt.pyplot.title('Hardcore')
+plt.pyplot.show()
